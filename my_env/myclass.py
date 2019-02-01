2ba50b52ad113d302eee2770cca2d052cf12982a
mukushevzhanarbek@gmail.com <mukushevzhanarbek@gmail.com>
2019-02-01 15:33:10 +0600

    added .py files

---
+++ my_env/myclass.py
@@ -0,0 +1,16 @@
+class Point
+    x = 2
+    y = 3
+    c = 4
+
+    def print_xyc(self):
+        print("x = ", self.x)
+        print("y = ", self.y)
+        print("c = ", self.c)
+
+if __name__ == "__main__":
+    p = Point()
+    p.print_xyc()
+    print(p.y,p.x,p.c)
+    print(p.x,p.y,p.c)
+    
